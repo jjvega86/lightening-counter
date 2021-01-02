ff439ab57de11936cdc8239282105d5e3c6e22e0
JJ <jj.vega86@gmail.com>
2021-01-02 12:00:09 -0500

    Added starting code to both LighteningCounter components

---
+++ src/components/LighteningCounter/lighteningCounter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './lighteningCounter.css';
+
+class LighteningCounter extends React.Component{
+    render(){
+        return(
+            <h1>Hello!</h1>
+        );
+    }
+}
+
+export default LighteningCounter;
--- src/components/LighteningCounterDisplay/lighteningCounterDisplay.jsx
+++ src/components/LighteningCounterDisplay/lighteningCounterDisplay.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import '../LighteningCounterDisplay/LighteningCounterDisplay.css'
+import LighteningCounter from '../LighteningCounter/lighteningCounter';
 
 class LighteningCounterDisplay extends React.Component{
-    state = {}
+    render(){
+        let divStyle = {
+            width: 250,
+            textAlign: "center",
+            backgroundColor: "black",
+            padding: 40,
+            fontFamily: "sans-serif",
+            color: "#999",
+            borderRadius: 10
+        };
+
+        return(
+            <div style={divStyle}>
+                <LighteningCounter />
+            </div>
+        )
+    }
 }
 
 export default LighteningCounterDisplay;
